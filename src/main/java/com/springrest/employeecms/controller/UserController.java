5971ed0cb1c0ae7255f49959c4fef92a7764a35b
imtiyaz-hirani <i.hirani@techskillsit.com>
2022-12-06 11:56:14 +0530

    verification commit

--- src/main/java/com/springrest/employeecms/controller/UserController.java
+++ src/main/java/com/springrest/employeecms/controller/UserController.java
@@ -3,6 +3,7 @@ package com.springrest.employeecms.controller;
 import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.springrest.employeecms.repository.UserRepository;
 
 @RestController
 @RequestMapping("/api/user")
+@CrossOrigin(origins = {"http://localhost:4200"})
 public class UserController {
 
 	@Autowired
